34e972b70499c53a3fc08475a6fa2dc50efd6dc0
zhangaqiang <2908487088@qq.com>
2023-12-16 11:50:31 +0800

    登录页面基础布局和表单校验

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -18,3 +18,8 @@ export function validUsername(str) {
   const valid_map = ['admin', 'editor']
   return valid_map.indexOf(str.trim()) >= 0
 }
+
+// 定义验证手机号的自定义校验函数
+export function validMoblie(str) {
+  return /^1[3-9]\d{9}$/.test(str)
+}
